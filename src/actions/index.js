a94a911189fc3f75055cef447b00f0a8ca640266
verissimohenry <verissimohenry04@gmail.com>
2021-09-06 15:00:50 +0100

    Fix linter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,20 @@
-/* eslint no-multiple-empty-lines: ["error", { "max": 2, "maxEOF": 0 }] */
 import * as actions from './booksActions';
 
-export const createBook = (title, category) => ({
-  type: actions.CREATE_BOOK,
-  payload: {
-    title,
-    category,
-  },
-});
-export const removeBook = (bookId) => ({
-  type: actions.REMOVE_BOOK,
-  payload: {
-    bookId,
-  },
-});
+export const createBook = (title, category) => (
+  {
+    type: actions.CREATE_BOOK,
+    payload: {
+      title,
+      category,
+    },
+  }
+);
+
+export const removeBook = (bookId) => (
+  {
+    type: actions.REMOVE_BOOK,
+    payload: {
+      bookId,
+    },
+  }
+);
