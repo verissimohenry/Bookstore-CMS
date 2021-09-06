23b0c95309a0cd2e89bd61698eb754e30084b563
verissimohenry <verissimohenry04@gmail.com>
2021-09-06 16:24:48 +0100

    MOdify file to correct error launching files

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = ({ bookId, title, category }) => (
-  <>
+  <tr>
     <td>{bookId}</td>
     <td>{title}</td>
     <td>{category}</td>
-  </>
+  </tr>
 );
 
 Book.propTypes = {
--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
 const BooksList = ({ bookId, title, category }) => (
-  <>
-    <tr>
-      <th>BookId</th>
-      <th>Title</th>
-      <th>Category</th>
-      <Book bookId={bookId} title={title} category={category} />
-    </tr>
-  </>
+  <tr>
+    <th>BookId</th>
+    <th>Title</th>
+    <th>Category</th>
+    <Book bookId={bookId} title={title} category={category} />
+  </tr>
 );
 
 BooksList.propTypes = {
