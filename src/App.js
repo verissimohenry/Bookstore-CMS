c0287d3156fb6c09ddd001f65cda0252eebf3fd5
verissimohenry <verissimohenry04@gmail.com>
2021-09-06 11:08:02 +0100

    Modify App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import BooksList from './container/BooksList';
+import BooksForm from './container/BooksForm';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Bookstore Verissimo</h2>
+    <div>
+      <h2>BookStore</h2>
+      <table className="App">
+        <tbody>
+          <BooksList />
+        </tbody>
+      </table>
+      <BooksForm />
     </div>
   );
 }
