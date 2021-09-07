07f236cd0cafa849b2da5d9f629cb7b43d20901b
verissimohenry <verissimohenry04@gmail.com>
2021-09-07 11:12:45 +0100

    Fix bookslist.js file for TSE correction

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -1,31 +1,24 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
-const BooksList = ({ bookId, title, category }) => (
-  <tr>
-    <th>BookId</th>
+const BooksList = ({ books }) => (
+  <table>
+    <th>Book ID</th>
     <th>Title</th>
-    <th>Category</th>
-    <Book bookId={bookId} title={title} category={category} />
-  </tr>
+    <th>category</th>
+    {books.map((book) => (
+      <Book
+        key={book.bookId}
+        bookId={book.bookId}
+        title={book.title}
+        category={book.category}
+      />
+    ))}
+  </table>
 );
 
-BooksList.propTypes = {
-  bookId: PropTypes.number,
-  title: PropTypes.string,
-  category: PropTypes.string,
-};
+const mapStateToProps = (state) => ({ books: state.bookReducer });
 
-BooksList.defaultProps = {
-  bookId: Math.random(),
-  title: '',
-  category: '',
-};
-
-const mapStateToProps = (state) => ({
-  books: state.books,
-});
-
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, null)(BooksList);
