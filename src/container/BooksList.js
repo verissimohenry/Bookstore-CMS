dd8001187f192745f3c51a27b83a4b8f83d8646a
verissimohenry <verissimohenry04@gmail.com>
2021-09-07 17:46:14 +0100

    Modify apps

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -3,22 +3,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 
-const BooksList = ({ books }) => (
+const BooksList = ({ bookId, title, category }) => (
   <table>
-    <th>Book ID</th>
-    <th>Title</th>
-    <th>category</th>
-    {books.map((books) => (
-      <Book
-        key={books.bookId}
-        bookId={books.bookId}
-        title={books.title}
-        category={books.category}
-      />
-    ))}
+    <tr>
+      <td>BookId</td>
+      <td>Title</td>
+      <td>Category</td>
+      <Book bookId={bookId} title={title} category={category} />
+    </tr>
   </table>
 );
 
-const mapStateToProps = (state) => ({ books: state.bookReducer });
+BooksList.defaultProps = {
+  bookId: [],
+};
 
-export default connect(mapStateToProps, null)(BooksList);
+const mapStateToProps = (state) => ({
+  books: state.books,
+});
+
+export default connect(mapStateToProps)(BooksList);
