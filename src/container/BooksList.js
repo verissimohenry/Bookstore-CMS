cec2d73368caf104d28261c01cb2c98ca080763d
verissimohenry <verissimohenry04@gmail.com>
2021-09-08 17:17:21 +0100

    Modify app

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -1,16 +1,17 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 
-const BooksList = ({ bookId, title, category }) => (
+const BooksList = ({ books }) => (
   <table>
-    <tr>
-      <td>BookId</td>
-      <td>Title</td>
-      <td>Category</td>
-      <Book bookId={bookId} title={title} category={category} />
-    </tr>
+    <td>
+      <tr>
+        <td>BookId</td>
+        <td>Title</td>
+        <td>Category</td>
+      </tr>
+    </td>
+    { books.map((book) => <tr><td><Book bookId={book.id} title={book.title} category={book.category} /></td></tr>)}
   </table>
 );
 
