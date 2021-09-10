b5bc7a0f1ca95e404ec8e3d22e68251020937cc6
verissimohenry <verissimohenry04@gmail.com>
2021-09-10 11:04:58 +0100

    Modify index files

--- src/index.js
+++ src/index.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers/indexReducer';
+import store from './reducers/indexReducer';
 import './index.css';
 import App from './App';
 
-const initialState = {
-  books: [{
-    bookId: Math.random().toString(),
-    title: 'Adam\'s Life',
-    category: 'Biography',
-  },
-  {
-    bookId: Math.random().toString(),
-    title: 'Football Heroes',
-    category: 'Sports',
-  },
-  {
-    bookId: Math.random().toString(),
-    title: 'Cricket History in India',
-    category: 'History',
-  }],
-};
-
-const store = createStore(rootReducer, initialState);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
