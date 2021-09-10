a143d8bd3b58c7334146785aca2129a9490c6fab
verissimohenry <verissimohenry04@gmail.com>
2021-09-10 09:04:06 +0100

    Modify  booksreducer.js file

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,17 +1,35 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/booksActions';
 
-const booksReducer = (state = {}, action) => {
+const initialState = {
+  books: [{
+    id: Math.floor(Math.random() * 30),
+    title: 'Dertogada',
+    category: 'Fiction',
+  },
+  {
+    id: Math.floor(Math.random() * 30),
+    title: 'React-redux',
+    category: 'Learning',
+
+  },
+
+  ],
+};
+
+const bookReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return [
-        ...state,
-        action.payload,
-      ];
+      return { books: [...state.books, action.book] };
+
     case REMOVE_BOOK:
-      return state.filter((e) => e !== action.payload);
+      return {
+        ...state,
+        books: state.books.filter((item) => item !== action.payload),
+      };
+
     default:
       return state;
   }
 };
 
-export default booksReducer;
+export default bookReducer;
