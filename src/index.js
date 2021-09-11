849bcd2cd73ffaa07c9f91eaafd3e41e5a2e6d12
verissimohenry <verissimohenry04@gmail.com>
2021-09-11 15:49:25 +0100

    Modify app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './reducers/indexReducer';
+import { createStore } from 'redux';
+import rootReducer from './reducers/indexReducer';
 import './index.css';
 import App from './App';
+import { initialState } from './reducers/filter';
+
+const store = createStore(rootReducer, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
