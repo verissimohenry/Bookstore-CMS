39163efac2c9f69b13b68eb831dbdfeee9ae0500
verissimohenry <verissimohenry04@gmail.com>
2021-09-11 12:04:36 +0100

    Modify bookslist.js file

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -3,61 +3,38 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook, changeFilter } from '../actions';
-import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = (props) => {
   const {
-    books, removeBook, filterBook, filter1,
+    books, removeBook, filter1,
   } = props;
-
   const handleRemoveBook = (book) => {
     removeBook(book);
   };
 
-  const handleFilterChange = (filter) => {
-    filterBook(filter);
-  };
-
   return (
-    <div>
-      <div>
-        <CategoryFilter filterChange={handleFilterChange} />
-        <div>
-          {filter1}
-        </div>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>BookId</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            books.filter((book) => book.category === filter1 || filter1 === 'All' || filter1 === '').map((book) => (
-              <Book
-                book={book}
-                key={book.bookId}
-                title={book.title}
-                category={book.category}
-                handleRemoveBook={(bookId) => handleRemoveBook(bookId)}
-                filter={filter1}
-              />
-            ))
-          }
-        </tbody>
-      </table>
-    </div>
+    <>
+      <main>
+        {
+books.filter((book) => book.category === filter1 || filter1 === 'All' || filter1 === '').map((book) => (
+  <Book
+    book={book}
+    key={book.bookId}
+    title={book.title}
+    category={book.category}
+    handleRemoveBook={(bookId) => handleRemoveBook(bookId)}
+    filter={filter1}
+  />
+))
+}
+      </main>
+    </>
   );
 };
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object),
   removeBook: PropTypes.func.isRequired,
-  filterBook: PropTypes.func.isRequired,
   filter1: PropTypes.string,
 };
 
