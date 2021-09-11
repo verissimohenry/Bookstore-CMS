3d2af01cc98c606310501f2a2efd9c56952104e3
verissimohenry <verissimohenry04@gmail.com>
2021-09-11 15:37:45 +0100

    Modify app to TSE correction

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,71 +1,51 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
-import circle from '../circle.png';
 
-const Book = ({
-  bookId, title, category, handleClick,
-}) => (
-  <div className="book-item font-robotoslab">
-    <div className="book-data">
-      <p className="category font-montserrat">{category}</p>
-      <p className="title font-montserrat">{title}</p>
-      <p className="author">Author Name</p>
-      <div className="links">
-        <button type="button" className="link">
-          Comments
-        </button>
-        <span className="tick" />
-        <button
-          className="link"
-          type="button"
-          onClick={() => {
-            handleClick({ bookId, title, category });
-          }}
-        >
-          Remove
-        </button>
-        <span className="tick" />
-        <button type="button" className="link">
-          Edit
-        </button>
+const Book = ({ book, handleRemoveBook }) => (
+  <div>
+    <div className="Lesson-Panel">
+      <div className="Lesson-Panel-left">
+        <div className="category">{book.category}</div>
+        <div className="title">{book.title}</div>
+        <div className="author">Author Name</div>
+        <div className="links">
+          <div className="link comments">Comments</div>
+          <div className="link remove">
+            <input
+              className="Remove"
+              type="submit"
+              onClick={() => handleRemoveBook(book)}
+              value="Remove"
+            />
+          </div>
+          <div className="link edit">Edit</div>
+        </div>
       </div>
-    </div>
-    <div className="reading-progress">
-      <img className="oval" alt="circle" src={circle} />
-      <div>
-        <p className="percent-complete font-montserrat">
-          {Math.floor(Math.random() * 100)}
-          %
-        </p>
-        <p className="completed font-montserrat">Completed</p>
+
+      <div className="Lesson-Panel-middle">
+        <div className="progress-circle">
+          <div className="circle" />
+        </div>
+        <div className="progress-info">
+          <div className="progress-info-percentage">{`${Math.floor(Math.random() * 100)}%`}</div>
+          <div className="progress-info-status">Completed</div>
+        </div>
       </div>
-    </div>
-    <div className="line-2" />
-    <div className="chapter-info">
-      <p className="current-chapter font-robotoslab">CURRENT CHAPTER</p>
-      <p className="chapter font-robotoslab">
-        Chapter
-        {Math.floor(Math.random() * 20)}
-      </p>
-      <button type="button" className="update-progress font-robotoslab">
-        UPDATE CHAPTER
-      </button>
+
+      <div className="line" />
+
+      <div className="Lesson-Panel-right">
+        <div className="chapter">Current chapter</div>
+        <div className="chapter-number">Chapter 17</div>
+        <div className="chapter-progress-button">Update Progress</div>
+      </div>
     </div>
   </div>
 );
 
 Book.propTypes = {
-  handleClick: PropTypes.func,
-  book: PropTypes.shape({
-    bookId: PropTypes.number,
-    title: PropTypes.string,
-    category: PropTypes.string,
-  }),
-};
-Book.defaultProps = {
-  book: null,
-  handleClick: null,
+  book: PropTypes.objectOf(PropTypes.string).isRequired,
+  handleRemoveBook: PropTypes.func.isRequired,
 };
 
 export default Book;
